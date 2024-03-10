ac59ad7b153ebc9c5f6ea619ac54c680871b2474
codemaestro730 <secretsuperdev730@gmail.com>
2024-03-10 19:11:49 +0100

    feat: implement auth module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as express from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  app.setGlobalPrefix(configService.get('API_PREFIX'));
+
+  app.use(express.static('public'));
+
+  const port = configService.get<number>('PORT');
+  await app.listen(port).then(() => {
+    console.log(`API server started on port ${port}`);
+  });
 }
 bootstrap();
